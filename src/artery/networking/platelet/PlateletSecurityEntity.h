7abbd7ce8cb0973ae667f8095235f0825859a3d3
Dimitri Simon <dimitri.simon@telecom-sudparis.eu>
2026-05-22 14:09:11 +0200

    fix: custom security entity for vanetza upgrade

--- src/artery/networking/platelet/PlateletSecurityEntity.h
+++ src/artery/networking/platelet/PlateletSecurityEntity.h
@@ -4,14 +4,14 @@
 #include <omnetpp/csimplemodule.h>
 #include <vanetza/common/runtime.hpp>
 #include <vanetza/common/position_provider.hpp>
-#include <vanetza/security/v2/backend.hpp>
+#include <vanetza/security/backend.hpp>
 #include <vanetza/security/v2/certificate_cache.hpp>
 #include <vanetza/security/v2/certificate_provider.hpp>
 #include <vanetza/security/v2/certificate_validator.hpp>
-#include <vanetza/security/v2/security_entity.hpp>
+#include <vanetza/security/security_entity.hpp>
 #include <vanetza/security/v2/sign_header_policy.hpp>
 #include <vanetza/security/v2/sign_service.hpp>
-#include <vanetza/security/v2/verify_service.hpp>
+#include <vanetza/security/verify_service.hpp>
 #include <memory>
 #include <string>
 
